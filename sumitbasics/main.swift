3ab9fe4b41aa7bc714c004d43b94770e3ebadcf3
MacStudent <macstudent@126-iMac04.local>
2019-02-07 21:05:56 -0500

    first

--- sumitbasics/main.swift
+++ sumitbasics/main.swift
@@ -10,3 +10,28 @@ import Foundation
 
 print("Hello, World!")
 
+
+
+var _ = 100
+var p = (100,200)
+print(p.0)
+var q = (a: 10, b : 20)
+print(q.a)
+var r = (a:10, (100,100))
+print(r.1.0)
+var t = p
+var (_, x1) = q
+print(x1)
+
+var e = (501,"Not")
+print("the name of e is: \(e)")
+
+
+if (p==q){
+    print("Same")
+}
+else
+{
+    print("getlost")
+}
+
