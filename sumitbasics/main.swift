dd446035a05d6580d3b543f19474770c229b0c50
MacStudent <macstudent@126-iMac04.local>
2019-02-07 20:05:20 -0500

    Initial Commit

---
+++ sumitbasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sumitbasics
+//
+//  Created by MacStudent on 2019-02-07.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
